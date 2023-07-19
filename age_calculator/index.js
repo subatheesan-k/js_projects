3213b57942b07aa8a825071b27dc5702b2cac7d9
subatheesan-k <subatheesandev@gmail.com>
2023-07-19 18:37:30 +0530

    sixth commit

--- age_calculator/index.js
+++ age_calculator/index.js
@@ -3,7 +3,20 @@ const birthdayEl = document.getElementById('birthday');
 
 function calculateAge(){
     const birthdayValue = birthdayEl.value;
-    console.log(birthdayValue);
+    
+    if(birthdayValue == ''){
+        alert('Please enter your birthday');
+    }else{
+        const age = getAge(birthdayValue);
+    }
+}
+
+function getAge(birthdayValue){
+    const currentDate = new Date();
+    const birthdayDate = new Date(birthdayValue);
+
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear()
+    console.log(age);
 }
 
 btnEl.addEventListener('click',calculateAge);
